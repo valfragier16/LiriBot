52a922e0f32fe54c4789b3f4812417fa3b118c65
Valerie Fragier <valfragier@gmail.com>
2018-10-09 14:40:22 -0400

    updated

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@
 // * `movie-this`
 // * `do-what-it-says`
 
-//not ready for this yet
+//code to read and set any environment variables with the dotenv package
 require("dotenv").config();
 
 //movie specific variables
